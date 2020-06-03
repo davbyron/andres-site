a023aaf01b628e2284da2b084dded0126f157789
David Byron <byron@MacBook-Pro.local>
2020-06-03 10:03:18 -0500

    Updates

---
+++ precache-manifest.b539cd390e1f18e20a138765d4332812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "830f521a8770ab57e1c4c7a51bcc0c2a",
+    "url": "/andres-site/index.html"
+  },
+  {
+    "revision": "6af5af5aef7b47099396",
+    "url": "/andres-site/static/css/main.452420b1.chunk.css"
+  },
+  {
+    "revision": "586bf3ecf91c11eb9967",
+    "url": "/andres-site/static/js/2.1325c115.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/andres-site/static/js/2.1325c115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af5af5aef7b47099396",
+    "url": "/andres-site/static/js/main.7b69de8b.chunk.js"
+  },
+  {
+    "revision": "5c43aee2cb5157a3d850",
+    "url": "/andres-site/static/js/runtime-main.bcd6a482.js"
+  }
+]);
