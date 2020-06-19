f60def1ec5d00968d466949013597295becd9f49
David Byron <byron@MacBook-Pro.local>
2020-06-19 13:59:34 -0500

    Updates

---
+++ precache-manifest.b2442ea8be0aad6f372367924f1d468e.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae1f8d2145563b3d9549c4c31962640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e5766a72480a0be124",
+    "url": "/static/css/main.16c3f1e9.chunk.css"
+  },
+  {
+    "revision": "b4ab74d2ba9bdbb47918",
+    "url": "/static/js/2.d63da729.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.d63da729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e5766a72480a0be124",
+    "url": "/static/js/main.4af060b0.chunk.js"
+  },
+  {
+    "revision": "6d63fec102e6a3baa05b",
+    "url": "/static/js/runtime-main.421f7cdb.js"
+  },
+  {
+    "revision": "1739ac2b2092e5bcc6c15dca31ed0630",
+    "url": "/static/media/amharic.1739ac2b.png"
+  },
+  {
+    "revision": "4df71bb7c6dc57465753ce45a6936032",
+    "url": "/static/media/arabic_jd.4df71bb7.png"
+  },
+  {
+    "revision": "27d782f7b6c9f646fbdcf6390497d05d",
+    "url": "/static/media/arabic_sibilants.27d782f7.pdf"
+  },
+  {
+    "revision": "ac5581638d04c4a0acb937ca8a40cad1",
+    "url": "/static/media/bahasa.ac558163.png"
+  },
+  {
+    "revision": "d67e2579aaf5e5465b215acd76777201",
+    "url": "/static/media/baluchi.d67e2579.png"
+  },
+  {
+    "revision": "4f1074a6ac720daaba5cc15298711009",
+    "url": "/static/media/bridgegate.4f1074a6.pdf"
+  },
+  {
+    "revision": "610f128e88f244283426186f73b498b9",
+    "url": "/static/media/cantonese.610f128e.png"
+  },
+  {
+    "revision": "ae5851eb77e89e58334fbbe9bae7d954",
+    "url": "/static/media/cv.ae5851eb.pdf"
+  },
+  {
+    "revision": "1ce6af423664846de19d22fe47809e4c",
+    "url": "/static/media/english_m.1ce6af42.png"
+  },
+  {
+    "revision": "77b1e2f836527da1416e318a7b1918b3",
+    "url": "/static/media/french.77b1e2f8.png"
+  },
+  {
+    "revision": "f050b5322129ad4e9245fbde5aee9cec",
+    "url": "/static/media/fribourg.f050b532.pdf"
+  },
+  {
+    "revision": "1a4cb1014f93a76c7e327680bd1b9092",
+    "url": "/static/media/german_fem.1a4cb101.png"
+  },
+  {
+    "revision": "6f797e748734d6ad77642fd02c5be9a7",
+    "url": "/static/media/greek.6f797e74.png"
+  },
+  {
+    "revision": "f9faea3cac13faf5bc94a505e847bfd8",
+    "url": "/static/media/italian.f9faea3c.png"
+  },
+  {
+    "revision": "e39957fe1d1bbd688dce95825f7cf0c9",
+    "url": "/static/media/japanese.e39957fe.png"
+  },
+  {
+    "revision": "487538dd23ab83cfa9dd9d3b59586a4e",
+    "url": "/static/media/kabardian.487538dd.png"
+  },
+  {
+    "revision": "580c532b2f5f001ddcedbb2d4b0a7b3e",
+    "url": "/static/media/kagayanen.580c532b.png"
+  },
+  {
+    "revision": "5006e5574c6a16d92b60657780ece575",
+    "url": "/static/media/kazakh.5006e557.png"
+  },
+  {
+    "revision": "55203fe27cf3d7658bbe70fde764c9ba",
+    "url": "/static/media/korean.55203fe2.png"
+  },
+  {
+    "revision": "5071c7496e829bb5a4bf6567c5247453",
+    "url": "/static/media/luo.5071c749.png"
+  },
+  {
+    "revision": "d764aa3cc6a7b5f06fc218eb0dab1751",
+    "url": "/static/media/maasai.d764aa3c.png"
+  },
+  {
+    "revision": "ba8c0d79035fc35bf455b12655aa8363",
+    "url": "/static/media/mixtec.ba8c0d79.png"
+  },
+  {
+    "revision": "e20f6be0c6df96369bccee18bc3e6187",
+    "url": "/static/media/muskogee_creek.e20f6be0.png"
+  },
+  {
+    "revision": "d1e6a883c91327e5911d8003991fb984",
+    "url": "/static/media/nahuatl.d1e6a883.png"
+  },
+  {
+    "revision": "f61880eb975dc473544642a6a0d09631",
+    "url": "/static/media/nambo.f61880eb.png"
+  },
+  {
+    "revision": "e4dca26e8608297f742dc0f047bae61b",
+    "url": "/static/media/portuguese_br.e4dca26e.png"
+  },
+  {
+    "revision": "2c8157557ed8a02112730e228ef3e197",
+    "url": "/static/media/portuguese_eu.2c815755.png"
+  },
+  {
+    "revision": "0b59ded4e9a82e1e72a76659d4188c01",
+    "url": "/static/media/qiang_hongyan.0b59ded4.png"
+  },
+  {
+    "revision": "758ae664db73eefbef6d70942377bc65",
+    "url": "/static/media/queens.758ae664.pdf"
+  },
+  {
+    "revision": "18bd512b46b9cecb413ba15a83667857",
+    "url": "/static/media/russian.18bd512b.png"
+  },
+  {
+    "revision": "c90e47dd1306758dc324ff68294809bf",
+    "url": "/static/media/russian_consonants.c90e47dd.png"
+  },
+  {
+    "revision": "821dae26d3232822382c395089115022",
+    "url": "/static/media/spanish.821dae26.png"
+  },
+  {
+    "revision": "5b998c777fef764b387986f7f5bff918",
+    "url": "/static/media/tanana_athabaskan.5b998c77.png"
+  },
+  {
+    "revision": "2af271d53f10de7025b7ae1c389de7d8",
+    "url": "/static/media/thai.2af271d5.png"
+  },
+  {
+    "revision": "d8cfbd1dc95a627e039130425a4b6d3f",
+    "url": "/static/media/thesis.d8cfbd1d.pdf"
+  },
+  {
+    "revision": "71134197a1a7333686dd0ee53c9eb018",
+    "url": "/static/media/vietnamese.71134197.png"
+  },
+  {
+    "revision": "bdb8e99e8ede1e2939a5769aa8ec1d3c",
+    "url": "/static/media/vowels_bib.bdb8e99e.pdf"
+  },
+  {
+    "revision": "cdace88aabac8d5b625c704969a2a1bb",
+    "url": "/static/media/welsh.cdace88a.png"
+  }
+]);
