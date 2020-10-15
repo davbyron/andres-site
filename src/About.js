ed8b4fa0c3e16233a8e2df612d2a9aa4c3e61406
David Byron <38707619+davbyron@users.noreply.github.com>
2020-10-15 08:35:18 -0500

    update about

--- src/About.js
+++ src/About.js
@@ -7,10 +7,14 @@ function About(props) {
       <div className="title">about</div>
       <div className="body">
         <p>
-          I'm a student at Boston University (BU) in the Graduate School
-          of Arts and Science pursuing a doctorate (PhD) in Linguistics.
-          My academic interests include sociolinguistics, phonetics, phonology,
+          I'm a student at Boston University (BU) in the Graduate School of Arts
+          and Science pursuing a doctorate (PhD) in Linguistics. My academic
+          interests include sociolinguistics, phonetics, phonology,
           migration studies, and understudied languages and language varieties.
+          My biggest current projects are documenting Cua (Kalahari Khoe) in
+          Botswana and exploring microcomparative variation in affectionate
+          kinship constructions across South Bantu languages which seem to rely
+          on inalienable syntactic structures.
         </p>
       </div>
     </section>
