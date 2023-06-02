6a8df6899a2b559f82d64515c56ae7874fd95e48
David Byron <davbyron@icloud.com>
2023-06-02 17:01:31 -0400

    make languages chart more readable on mobile

--- src/Languages.js
+++ src/Languages.js
@@ -63,8 +63,32 @@ class CustomizedAxisTick extends React.Component {
   }
 }
 
+// The following is adapted from: https://recharts.org/en-US/examples/CustomizedLabelLineChart
+class CustomizedXAxisTick extends React.Component {
+  render() {
+    const {
+      x, y, stroke, payload,
+    } = this.props;
+
+    console.log(x)
+    console.log(y)
+    console.log(payload)
+    console.log(stroke)
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={5} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+      </g>
+    );
+  }
+}
+
 function Languages(props) {
   const [focusBar, setFocusBar] = useState(null);
+
+  const screenOrientation = window.screen.orientation.type;
+  console.log(screenOrientation)
+  console.log(screenOrientation.startsWith('landscape'))
 
   const data = [
     { name: 'Arabic (MSA, Palestinian)', alt: 'Arabic', level: 3 },
@@ -89,45 +113,95 @@ function Languages(props) {
   return (
     <section className="languages">
       <h2>languages</h2>
-      <div className="chart">
-        <ResponsiveContainer
-          width="100%"
-          height="70%">
-          <BarChart
-            data={data}
-            margin={{ bottom: 85 }}
-            onMouseMove={state => {
-              if (state.isTooltipActive) {
-                setFocusBar(state.activeTooltipIndex);
-              } else {
-                setFocusBar(null);
-              }
-            }}>
-            <Tooltip formatter={(value, name, props) => { return levelFormatter(value) }} />
-            <XAxis
-              type="category"
-              dataKey="name"
-              interval={0}
-              tick={<CustomizedAxisTick />}
-              tickLine={false}
+      {/* Landscape */ }
+      {(screenOrientation.startsWith('landscape') &&
+        <div className="chart">
+          <ResponsiveContainer
+            width="100%"
+            height="70%">
+            <BarChart
+              data={data}
+              margin={{ bottom: 85 }}
+              onMouseMove={state => {
+                if (state.isTooltipActive) {
+                  setFocusBar(state.activeTooltipIndex);
+                } else {
+                  setFocusBar(null);
+                }
+              }}>
+              <Tooltip formatter={(value, name, props) => { return levelFormatter(value) }} />
+              <XAxis
+                type="category"
+                dataKey="name"
+                interval={0}
+                tick={<CustomizedAxisTick />}
+                tickLine={false}
               />
-            <YAxis
-              type="number"
-              dataKey="level"
-              domain={[0,5]}
-              ticks={[1, 2, 3, 4, 5]}
-              tickFormatter={levelFormatter}
-              width={150} />
-            <Bar
-              dataKey="level"
-              isAnimationActive={false}>
-              {data.map((entry, index) => (
-                <Cell fill={whichColor(entry['level'])} />
-              ))}
-            </Bar>
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+              <YAxis
+                type="number"
+                dataKey="level"
+                domain={[0,5]}
+                ticks={[1, 2, 3, 4, 5]}
+                tickFormatter={levelFormatter}
+                width={150}
+              />
+              <Bar
+                dataKey="level"
+                isAnimationActive={false}>
+                {data.map((entry, index) => (
+                  <Cell fill={whichColor(entry['level'])} />
+                ))}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      )}
+      {/* Portrait */ }
+      {(screenOrientation.startsWith('portrait') &&
+        <div className='chart'>
+          <ResponsiveContainer
+            width="100%"
+            height="200%">
+            <BarChart
+              data={data}
+              layout='vertical'
+              margin={{ left: 60, right: 30, bottom: 100 }}
+              onMouseMove={state => {
+                if (state.isTooltipActive) {
+                  setFocusBar(state.activeTooltipIndex);
+                } else {
+                  setFocusBar(null);
+                }
+              }}
+            >
+              <Tooltip formatter={(value, name, props) => { return levelFormatter(value) }} />
+              <XAxis
+                type="number"
+                dataKey="level"
+                domain={[0,5]}
+                angle={-35}
+                tickMargin={20}
+                ticks={[1, 2, 3, 4, 5]}
+                tickFormatter={levelFormatter}
+              />
+              <YAxis
+                type="category"
+                dataKey="name"
+                interval={0}
+                tickLine={false}
+              />
+              <Bar
+                dataKey="level"
+                isAnimationActive={false}
+              >
+                {data.map((entry, index) => (
+                  <Cell fill={whichColor(entry['level'])} />
+                ))}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      )}
     </section>
   )
 }
